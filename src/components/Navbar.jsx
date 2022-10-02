61e563080f5dec4284c8107d5b7739a6b983fcb9
Lucho Web <luchoweb.dev@gmail.com>
2022-10-02 17:02:48 -0500

    responsive menu done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import { translate } from '../utils'
 
 import "../styles/components/Navbar.scss"
 
 export default function Navbar() {
+  const [ showMenu, setShowMenu ] = useState(false);
   const links = [
     {
       label: translate('home'),
@@ -24,7 +26,20 @@ export default function Navbar() {
 
   return (
     <nav className="nav-bar">
-      <ul className="list-unstyled menu m-0 p-0 d-flex flex-wrap align-items-center gap-4 justify-content-center justify-content-md-end">
+      <div className="burger" onClick={() => setShowMenu(!showMenu)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <span
+        className={`close-menu ${showMenu ? 'show' : ''}`}
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        &#10005;
+      </span>
+
+      <ul className={`list-unstyled menu m-0 p-0 d-md-flex flex-wrap align-items-center gap-4 justify-content-center justify-content-md-end ${showMenu ? 'show' : ''}`}>
         { links.map(link => (
           <li key={`k-${link.href}`}>
             <a href={link.href}>
